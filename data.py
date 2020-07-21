26dfa3d61fc83b272e8578ae8de89ce36cd532a3
ytmiao <ytmiao@ucla.edu>
2020-07-20 17:01:08 -0700

    data class

---
+++ data.py
@@ -0,0 +1,82 @@
+from functions import (deconvsk, finterp, ldrc, masks, reconstruction)
+
+class Data:
+    def __init__(self, filepath, filename):
+        self.filename = filename
+        self.filepath = filepath
+        self.ave = None
+        self.moments_set = None
+        self.cumulants_set = None
+        self.morder = 0
+        self.corder = 0
+        
+    # TODO: get movie length
+    # def: get_length(self):
+        
+    def average_image(self):
+        self.ave = reconstruction.average_image(self.filepath, self.filename)
+        return self.ave
+    
+    def moments_images(self, highest_order = 6):
+        self.morder = highest_order
+        self.moments_set = reconstruction.calc_moments(self.filepath, self.filename, highest_order)
+        return self.moments_set
+    
+    def cumulants_images(self, highest_order = 6, m_set = None, same_order = True):
+        self.corder = highest_order
+        if m_set is None:
+            if self.moments_set is not None and self.morder >= self.corder: 
+                m_set = self.moments_set
+            else:
+                m_set = self.moments_images(highest_order)
+              
+        if same_order is False:
+            m_set = self.moments_images(highest_order)
+            
+        self.cumulants_set = reconstruction.calc_cumulants_from_moments(m_set, highest_order)
+        return self.cumulants_set
+            
+    def ldrc(self, order = 6, window_size = 25, mask_im = None, input_im = None):      
+        if input_im is None: 
+            if self.moments_set is not None:
+                if self.morder >= order:
+                    input_im = self.moments_set[order]
+                else:
+                    self.moments_images(order)[order]
+            else:
+                self.moments_images(order)[order]
+                
+        if mask_im is None:
+                mask_im = self.average_image()                
+
+        # input_im = self.moments_set[4], mask_im = self.ave    
+        self.ldrc_image = ldrc.ldrc(mask_im, input_im, order, window_size)
+        return self.ldrc_image
+    
+    def deconvsk(self, est_psf = masks.gauss2D_mask((51, 51), 2), input_im = None, deconv_lambda = 1.5, deconv_iter = 20):
+        if input_im is None:
+            if self.moments_set is not None:
+                input_im = self.moments_set[morder]
+            elif self.ave is not None:
+                input_im = self.ave
+            else:
+                input_im = self.average_image()
+        self.deconv = deconvsk.deconvsk(est_psf, input_im, deconv_lambda, deconv_iter)
+        return self.deconv
+    
+    def finterp_tiffstack(self, interp_num_lst = [2,4], mvlength = None, save_option = True, return_option = False):
+        if return_option == False:
+            finterp.fourier_interp_tiffstack(self.filepath, self.filename, interp_num_lst, mvlength, save_option, return_option)
+        else:
+            self.finterps = finterp.fourier_interp_tiffstack(self.filepath, self.filename, interp_num_lst, mvlength, save_option, return_option)
+            return self.finterps
+       
+    def finterp_image(self, input_im = None, interp_num_lst = [2,4], save_option = False, filename = 'Image', return_option = True):
+        if input_im is None:
+            input_im = self.average_image()
+            
+        if return_option == False:
+            finterp.fourier_interp_tiffimage(input_im, interp_num_lst, save_option, filename, return_option)
+        else:
+            self.finterp = finterp.fourier_interp_tiffimage(input_im, interp_num_lst, save_option, filename, return_option)
+            return self.finterp
