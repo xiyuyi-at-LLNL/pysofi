04d0c911dc1b0c5b5fe8e6161965fc0248f1de3f
ytmiao <ytmiao@ucla.edu>
2020-07-28 22:53:48 -0700

    Rewrite RL deconvolution

--- functions/deconvsk.py
+++ functions/deconvsk.py
@@ -1,29 +1,11 @@
+from numpy.fft import (fftshift, ifftshift, fftn, ifftn, rfftn, irfftn)
 import numpy as np
-try:
-    import pyfftw
-    from pyfftw.interfaces.numpy_fft import (fftshift, ifftshift, fftn, ifftn, rfftn, irfftn)
-    # Turn on the cache for optimum performance
-    pyfftw.interfaces.cache.enable()
-except ImportError:
-    from numpy.fft import (fftshift, ifftshift, fftn, ifftn, rfftn, irfftn)
-import scipy.signal.signaltools as sig
-from scipy.signal import fftconvolve
-from scipy.ndimage import convolve
-
-def fft_pad(psf, out_shape, mode='constant'):
-    psf_shape = np.shape(psf)
-    shape_diff = (out_shape[0]-psf_shape[0], out_shape[1]-psf_shape[1])
-    # out_psf = np.pad(psf, ((0,shape_diff[0]), (0,shape_diff[1])), mode)
-    out_psf = np.pad(psf, ((shape_diff[0]//2+1,shape_diff[0]//2), (shape_diff[1]//2+1,shape_diff[1]//2)), mode)
-    return out_psf
-
-# the following functions are stolen from https://github.com/david-hoffman/pyDecon/tree/master/notebooks
 
 def _prep_img_and_psf(image, psf):
     """Do basic data checking, convert data to float, normalize psf and make
     sure data are positive"""
-    assert psf.ndim == image.ndim, ("image and psf do not have the same number"
-                                    " of dimensions")
+    #assert psf.ndim == image.ndim, ("image and psf do not have the same number"
+     #                               " of dimensions")
     image = image.astype(np.float)
     psf = psf.astype(np.float)
     # need to make sure both image and PSF are totally positive.
@@ -57,104 +39,180 @@ def _zero2eps(data):
     # make a copy of the data
     return np.fmax(data, np.finfo(data.dtype).eps)
 
-def _rl_core_matlab(image, otf, psf, u_t, **kwargs):
-    """The core update step of the RL algorithm
+# https://github.com/aboucaud/pypher/blob/master/pypher/pypher.py
+def zero_pad(image, shape, position='center'):
+    """
+    Extends image to a certain size with zeros
+    Parameters
+    ----------
+    image: real 2d `numpy.ndarray`
+        Input image
+    shape: tuple of int
+        Desired output shape of the image
+    position : str, optional
+        The position of the input image in the output one:
+            * 'corner'
+                top-left corner (default)
+            * 'center'
+                centered
+    Returns
+    -------
+    padded_img: real `numpy.ndarray`
+        The zero-padded image
+    """
+    shape = np.asarray(shape, dtype=int)
+    imshape = np.asarray(image.shape, dtype=int)
 
-    This is a fast but inaccurate version modeled on matlab's version
-    One improvement is to pad everything out when the shape isn't
-    good for fft."""
-    reblur = irfftn(otf * rfftn(u_t, u_t.shape, **kwargs), u_t.shape, **kwargs)
+    if np.alltrue(imshape == shape):
+        return image
+
+    if np.any(shape <= 0):
+        raise ValueError("ZERO_PAD: null or negative shape given")
+
+    dshape = shape - imshape
+    if np.any(dshape < 0):
+        raise ValueError("ZERO_PAD: target size smaller than source one")
+
+    pad_img = np.zeros(shape, dtype=image.dtype)
+
+    idx, idy = np.indices(imshape)
+
+    if position == 'center':
+        if np.any(dshape % 2 != 0):
+            raise ValueError("ZERO_PAD: source and target shapes "
+                             "have different parity.")
+        offx, offy = dshape // 2
+    else:
+        offx, offy = (0, 0)
+
+    pad_img[idx + offx, idy + offy] = image
+
+    return pad_img
+
+
+def psf2otf(psf, shape):
+    """
+    Convert point-spread function to optical transfer function.
+    Compute the Fast Fourier Transform (FFT) of the point-spread
+    function (PSF) array and creates the optical transfer function (OTF)
+    array that is not influenced by the PSF off-centering.
+    By default, the OTF array is the same size as the PSF array.
+    To ensure that the OTF is not altered due to PSF off-centering, PSF2OTF
+    post-pads the PSF array (down or to the right) with zeros to match
+    dimensions specified in OUTSIZE, then circularly shifts the values of
+    the PSF array up (or to the left) until the central pixel reaches (1,1)
+    position.
+    Parameters
+    ----------
+    psf : `numpy.ndarray`
+        PSF array
+    shape : int
+        Output shape of the OTF array
+    Returns
+    -------
+    otf : `numpy.ndarray`
+        OTF array
+    Notes
+    -----
+    Adapted from MATLAB psf2otf function
+    """
+    if np.all(psf == 0):
+        return np.zeros_like(psf)
+
+    inshape = psf.shape
+    # Pad the PSF to outsize
+    psf = zero_pad(psf, shape, position='corner')
+
+    # Circularly shift OTF so that the 'center' of the PSF is
+    # [0,0] element of the array
+    for axis, axis_size in enumerate(inshape):
+        psf = np.roll(psf, -int(axis_size / 2), axis=axis)
+
+    # Compute the OTF
+    otf = np.fft.fft2(psf)
+
+    # Estimate the rough number of operations involved in the FFT
+    # and discard the PSF imaginary part if within roundoff error
+    # roundoff error  = machine epsilon = sys.float_info.epsilon
+    # or np.finfo().eps
+    n_ops = np.sum(psf.size * np.log2(psf.shape))
+    otf = np.real_if_close(otf, tol=n_ops)
+
+    return otf
+
+def otf2psf(otf, shape):
+
+    if np.all(otf == 0):
+        return np.zeros_like(otf)
+
+    inshape = otf.shape
+    
+    # Compute the PSF
+    psf = np.fft.ifft2(otf)
+    
+    # Estimate the rough number of operations involved in the FFT
+    # and discard the PSF imaginary part if within roundoff error
+    # roundoff error  = machine epsilon = sys.float_info.epsilon
+    # or np.finfo().eps
+    n_ops = np.sum(otf.size * np.log2(otf.shape))
+    psf = np.real_if_close(psf, tol=n_ops)
+    psf=np.real(psf)
+    
+    
+    # Circularly shift PSF so that the 'center' of the PSF is
+    # [0,0] element of the array
+    for axis, axis_size in enumerate(shape):
+        psf = np.roll(psf, int(axis_size / 2), axis=axis)
+    
+    # Crop output array
+    psf = psf[0:shape[0], 0:shape[1]]
+
+    return psf
+
+def corelucy(image, H):
+    u_t = image
+    reblur = np.real(ifftn(H * fftn(u_t, u_t.shape), u_t.shape))
     reblur = _ensure_positive(reblur)
     im_ratio = image / reblur
-    estimate = irfftn(np.conj(otf) * rfftn(im_ratio, im_ratio.shape, **kwargs), im_ratio.shape, **kwargs)
-    # need to figure out a way to pass the psf shape
-    for i, (s, p) in enumerate(zip(image.shape, psf.shape)):
-        if s % 2 and not p % 2:
-            estimate = np.roll(estimate, 1, i)
-    estimate = _ensure_positive(estimate)
-    return u_t * estimate
+    f = fftn(im_ratio)
+    return f
 
-def _rl_accelerate(g_tm1, g_tm2, u_t, u_tm1, u_tm2, prediction_order):
-    """Biggs-Andrews Acceleration
+# image, psf = _prep_img_and_psf(ext_im, deconv_psf0)
 
-    .. [2] Biggs, D. S. C.; Andrews, M. Acceleration of Iterative Image
-    Restoration Algorithms. Applied Optics 1997, 36 (8), 1766."""
-    # TODO: everything here can be wrapped in ne.evaluate
-    if g_tm2 is not None:
-        alpha = (g_tm1 * g_tm2).sum() / ((g_tm2**2).sum() + np.finfo(g_tm1.dtype).eps)
-        alpha = max(min(alpha, 1), 0)
-    else:
-        alpha = 0
-    # print("lambda = {:.6f}".format(alpha))
-    # if alpha is positive calculate predicted step
-    if alpha:
-        # first order correction
-        h1_t = u_t - u_tm1
-        if prediction_order > 1:
-            # second order correction
-            h2_t = (u_t - 2 * u_tm1 + u_tm2)
-        else:
-            h2_t = 0
-        u_tp1 = u_t + alpha * h1_t + alpha**2 / 2 * h2_t
-        return u_tp1
-    else:
-        return u_t
+def richardson_lucy(image, psf, iterations=10, **kwargs):
     
-def richardson_lucy(image, psf, iterations=10, prediction_order=1,
-                    core_type="matlab", init="matlab", **kwargs):
+    # 1. prepare parameters
     image, psf = _prep_img_and_psf(image, psf)
-    # choose core
-    if core_type == "matlab":
-        core = _rl_core_matlab
-    else:
-        raise ValueError("{} is not an acceptable core type".format(core_type))
-    # set up the proper dict for the right core
-    if core is _rl_core_matlab:
-        image, psf = _prep_img_and_psf(image, psf)
-        if psf.shape != image.shape:
-            # its been assumed that the background of the psf has already been
-            # removed and that the psf has already been centered
-            psf = fft_pad(psf, image.shape, mode='constant')
-        otf = rfftn(ifftshift(psf))
-        core_dict = dict(image=image, otf=otf, psf=psf)
-    else:
-        raise RuntimeError("{} is not a valid core".format(core))
-    # initialize variable for iterations
-    # previous estimate
-    u_tm1 = u_tm2 = None
-    if init == "matlab":
-        core_dict["u_t"] = u_t = image
-    else:
-        # current estimate, for the initial estimate we use the mean of the
-        # data this promotes a smooth solution and helps to reduce noise.
-        core_dict["u_t"] = u_t = np.ones_like(image) * image.mean()
-    # previous difference
-    g_tm1 = g_tm2 = None
-    for i in range(iterations):
-        # if prediction is requested perform it
-        if prediction_order:
-            # need to save prediction as intermediate value
-            y = _rl_accelerate(g_tm1, g_tm2, u_t, u_tm1, u_tm2,
-                               prediction_order)
-        else:
-            y = u_t
-        # update estimate and ensure positive
-        core_dict["u_t"] = _ensure_positive(y)
-        # call the update function
-        u_tp1 = core(**core_dict, **kwargs)
-        # update
-        # update g's
-        g_tm2 = g_tm1
-        # this is where the magic is, we need to compute from previous step
-        # which may have been augmented by acceleration
-        g_tm1 = u_tp1 - y
-        # now move u's along
-        # Here we don't want to update with accelerated version.
-        # why not? is this a mistake?
-        u_t, u_tm1, u_tm2 = u_tp1, u_t, u_tm1
-        
-    # return final estimate
-    return u_t
+    sizeI, sizePSF = image.shape, psf.shape
+    J, P = {}, {}
+    J[1], J[2], J[3], J[4] = image, image, 0, np.zeros((np.prod(sizeI), 2))
+    P[1], P[2], P[3], P[4] = psf, psf, 0, np.zeros((np.prod(sizePSF), 2))
+    WEIGHT = np.ones(image.shape)
+    fw = fftn(WEIGHT)
+    
+    # 2. L_R iterations
+    for k in range(iterations):
+        # 2a. make image and PSF predictions for the next iteration
+        Y = np.maximum(J[2],0)
+        B = np.maximum(P[2],0)
+        B /= B.sum()
+        # 2b. make core for the LR estimation
+        H = psf2otf(B, sizeI) 
+        CC = corelucy(Y, H)
+        # 2c. Determine next iteration image & apply positivity constraint
+        J[3] = J[2]
+        scale = np.real(ifftn(np.multiply(np.conj(H),fw))) + np.sqrt(np.finfo(H.dtype).eps)
+        J[2] = np.maximum(np.multiply(image, np.real(ifftn(np.multiply(np.conj(H), CC))))/scale, 0)
+        J[4] = np.vstack([J[2].T.reshape(-1,) - Y.T.reshape(-1,), J[4][:,1]]).T
+        # 2d. Determine next iteration PSF & apply positivity constraint + normalization
+        P[3] = P[2]
+        H = fftn(J[3])
+        scale = otf2psf(np.multiply(np.conj(H),fw), sizePSF) + np.sqrt(np.finfo(H.dtype).eps)
+        P[2] = np.maximum(np.multiply(B, otf2psf(np.multiply(np.conj(H),CC), sizePSF))/scale, 0)
+        P[2] /= P[2].sum()
+        P[4] = np.vstack([P[2].T.reshape(-1,) - B.T.reshape(-1,), P[4][:,1]]).T
+    P, J = P[2], J[2]  # PSF and updated image
+    return P, J  
 
 def deconvsk(est_psf, input_im, deconv_lambda, deconv_iter):
     xdim, ydim = np.shape(input_im)
@@ -163,7 +221,7 @@ def deconvsk(est_psf, input_im, deconv_lambda, deconv_iter):
     psf0 = est_psf / np.max(est_psf)
     for iter_num in range(deconv_iter):
         alpha = deconv_lambda**(iter_num+1) / (deconv_lambda - 1)
-        deconv_im = richardson_lucy(deconv_im, psf0**alpha, 1)    
+        deconv_psf, deconv_im = richardson_lucy(deconv_im, psf0**alpha, 1)    
     
     deconv_im = deconv_im[0:xdim, 0:ydim]
     return deconv_im
