88e62edd076dbc0c1d856a80aca6a97b170c4a3e
yutingmiao <ytmiao@g.ucla.edu>
2020-08-04 23:28:08 -0700

    update moments calculation

--- functions/reconstruction.py
+++ functions/reconstruction.py
@@ -20,26 +20,26 @@ def average_image(filepath, filename):
     
     return mean_im
 
-def calc_moments(filepath, filename, highest_order):
+def calc_moments(filepath, filename, highest_order, current_order = 0, mean_im = None, m_set = {}):
     '''
     Get all moment-reconstructed images to the user-defined highest order for a video (tiff stack).
     '''
     # TODO: the user can define the number of sub-blocks, and get moment images for each bloack.
-    mean_im = average_image(filepath, filename)
-    xdim, ydim = np.shape(mean_im)
-    m_set = {}
+    if mean_im is None:
+        mean_im = average_image(filepath, filename)
 
     imstack = tiff.TiffFile(filepath + '/' + filename)
     xdim, ydim = np.shape(imstack.pages[0])
     mvlength = len(imstack.pages)
     
-    for order in range(highest_order):
-        m_set[order+1] = np.zeros((xdim, ydim))
-        for frame_num in range(mvlength):
-            im = tiff.imread(filepath + '/' + filename, key=frame_num)
-            m_set[order+1] = m_set[order+1] + np.power(im - mean_im, order+1)
+    if highest_order > current_order:
+        for order in range(current_order, highest_order):
+            m_set[order+1] = np.zeros((xdim, ydim))
+            for frame_num in range(mvlength):
+                im = tiff.imread(filepath + '/' + filename, key=frame_num)
+                m_set[order+1] = m_set[order+1] + np.power(im - mean_im, order+1)
         
-        m_set[order+1] = np.int64(m_set[order+1] / mvlength)
+            m_set[order+1] = np.int64(m_set[order+1] / mvlength)
     return m_set
 
 def calc_cumulants_from_moments(moment_set, highest_order):
