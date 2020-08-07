25605ebf3a93dc20946d4daa858f7cd0b728643e
ytmiao <ytmiao@ucla.edu>
2020-08-06 23:19:21 -0700

    rewrite moments calculation

--- functions/reconstruction.py
+++ functions/reconstruction.py
@@ -1,5 +1,4 @@
 import numpy as np
-import imageio
 import tifffile as tiff
 import scipy.special
 
@@ -21,26 +20,26 @@ def average_image(filepath, filename):
     
     return mean_im
 
-def calc_moments(filepath, filename, highest_order):
+def calc_moments(filepath, filename, highest_order, current_order = 0, mean_im = None, m_set = {}):
     '''
     Get all moment-reconstructed images to the user-defined highest order for a video (tiff stack).
     '''
     # TODO: the user can define the number of sub-blocks, and get moment images for each bloack.
-    mean_im = average_image(filepath, filename)
-    xdim, ydim = np.shape(mean_im)
-    m_set = {}
+    if mean_im is None:
+        mean_im = average_image(filepath, filename)
 
     imstack = tiff.TiffFile(filepath + '/' + filename)
     xdim, ydim = np.shape(imstack.pages[0])
     mvlength = len(imstack.pages)
     
-    for order in range(highest_order):
-        m_set[order+1] = np.zeros((xdim, ydim))
-        for frame_num in range(mvlength):
-            im = tiff.imread(filepath + '/' + filename, key=frame_num)
-            m_set[order+1] = m_set[order+1] + np.power(im - mean_im, order+1)
+    if highest_order > current_order:
+        for order in range(current_order, highest_order):
+            m_set[order+1] = np.zeros((xdim, ydim))
+            for frame_num in range(mvlength):
+                im = tiff.imread(filepath + '/' + filename, key=frame_num)
+                m_set[order+1] = m_set[order+1] + np.power(im - mean_im, order+1)
         
-        m_set[order+1] = np.int64(m_set[order+1] / mvlength)
+            m_set[order+1] = np.int64(m_set[order+1] / mvlength)
     return m_set
 
 def calc_cumulants_from_moments(moment_set, highest_order):
@@ -52,5 +51,3 @@ def calc_cumulants_from_moments(moment_set, highest_order):
         k_set[order] = np.int64(moment_set[order] - np.sum(np.array([scipy.special.comb(order-1,i)*k_set[order-i]*moment_set[i] for i in range(1,order)]),axis=0))
     
     return k_set
-
-
