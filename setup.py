ad2c5da8029a8687772b9d7b80fd404c34f325f7
xiyuyi-at-LLNL <72636294+xiyuyi-at-LLNL@users.noreply.github.com>
2021-12-11 13:17:39 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author = 'Yuting Miao, Shimon Weiss, Xiyu Yi',
     author_email = 'ytmiao@ucla.edu; yi10@llnl.gov',
     url = 'https://github.com/xiyuyi-at-LLNL/pysofi',
-    download_url = 'https://github.com/xiyuyi-at-LLNL/pysofi/archive/refs/tags/v0.1.0-alpha.tar.gz',
+    download_url = 'https://github.com/xiyuyi-at-LLNL/pysofi/archive/refs/tags/v0.1.1-alpha.tar.gz',
     keywords = ['pysofi'],
     classifiers = [],
 )
