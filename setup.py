efd3a767c9aafa01ad169bb65ecf201acad3bd69
xiyu <yi10@llnl.gov>
2021-12-11 13:47:41 -0800

    email

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     version = 'v0.1.1-alpha', 
     description = 'first release',
     author = 'Yuting Miao, Shimon Weiss, Xiyu Yi',
-    author_email = 'ytmiao@ucla.edu; yi10@llnl.gov',
+    author_email = 'ytmiao@ucla.edu',
     url = 'https://github.com/xiyuyi-at-LLNL/pysofi',
     download_url = 'https://github.com/xiyuyi-at-LLNL/pysofi/archive/refs/tags/v0.1.1-alpha.tar.gz',
     keywords = ['pysofi'],
