f662308a3aafc3a6571c99d19f282f06d95b1ec8
xiyuyi-at-LLNL <72636294+xiyuyi-at-LLNL@users.noreply.github.com>
2021-12-11 13:17:52 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name = 'pysofi',
     packages = ['pysofi'],
-    version = 'v0.1.0-alpha', 
+    version = 'v0.1.1-alpha', 
     description = 'first release',
     author = 'Yuting Miao, Shimon Weiss, Xiyu Yi',
     author_email = 'ytmiao@ucla.edu; yi10@llnl.gov',
