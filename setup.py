61aac9341e6f5fc59b3ff8c7937973300ace368f
xiyuyi-at-LLNL <72636294+xiyuyi-at-LLNL@users.noreply.github.com>
2021-12-11 10:36:10 -0800

    prepare for release (#16)
    prepare for release.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'pysofi',
+    packages = ['pysofi'],
+    version = 'v0.1.0-alpha', 
+    description = 'first release',
+    author = 'Yuting Miao, Shimon Weiss, Xiyu Yi',
+    author_email = 'ytmiao@ucla.edu; yi10@llnl.gov',
+    url = 'https://github.com/xiyuyi-at-LLNL/pysofi',
+    download_url = 'https://github.com/xiyuyi-at-LLNL/pysofi/archive/refs/tags/v0.1.0-alpha.tar.gz',
+    keywords = ['pysofi'],
+    classifiers = ['Intended Audience :: Science/Research'],
+)
